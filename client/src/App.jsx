e54d9f4655db94337196eb989db51884e85c5fee
Divesh Gaonkar <169079140+DiveshGaonkar99@users.noreply.github.com>
2024-10-11 17:09:20 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.baseURL = 'https://chat-app-backend-kpaw.onrender.com';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
